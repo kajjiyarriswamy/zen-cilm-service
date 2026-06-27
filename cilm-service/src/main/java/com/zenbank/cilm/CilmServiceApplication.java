769afab17bd7a974daa27013617ebd0bf3fc502a
yarriswamy <yarriswamy536@outlook.com>
2026-06-28 01:27:58 +0530

    Initial commit - CILM microservice

---
+++ cilm-service/src/main/java/com/zenbank/cilm/CilmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zenbank.cilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CilmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CilmServiceApplication.class, args);
+	}
+
+}
---
+++ cilm-service/src/test/java/com/zenbank/cilm/CilmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenbank.cilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CilmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
