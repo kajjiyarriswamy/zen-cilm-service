bf26a981d0f0a820dfccc42c842f502143a22afc
Satishpatta <satishpatta555@gmail.com>
2026-06-30 10:33:36 +0530

    Add the customer id

--- cilm-service/src/main/java/com/zenbank/cilm/entity/CustomerContact.java
+++ cilm-service/src/main/java/com/zenbank/cilm/entity/CustomerContact.java
@@ -1,5 +1,13 @@
 package com.zenbank.cilm.entity;
 
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 public class CustomerContact {
+	
+	 @ManyToOne(fetch = FetchType.LAZY, optional = false)
+	    @JoinColumn(name = "customer_id", nullable = false)
+	    private Customer customer;
 
 }
