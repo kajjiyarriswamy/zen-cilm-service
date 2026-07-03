93eea9669c7d5f1be6c38bb72c34247a5df91965
Gaddam Sudharshan <128913123+Sudharshan0903@users.noreply.github.com>
2026-07-03 09:40:40 +0530

    Revert "Implemented customer update service"

--- cilm-service/src/main/java/com/zenbank/cilm/controller/CustomerController.java
+++ cilm-service/src/main/java/com/zenbank/cilm/controller/CustomerController.java
@@ -147,21 +147,31 @@ public class CustomerController {
         return ResponseEntity.ok(ApiResponseUtil.success(saved));
     }
     
-    
-    @PutMapping("/{customerId}")
-    public ResponseEntity<Map<String, Object>> updateCustomer(
-            @PathVariable Long customerId,
-            @RequestBody CustomerRequestDto requestDto) {
-
-        customerService.updateCustomer(customerId, requestDto);
-
-        return ResponseEntity.ok(
-                ApiResponseUtil.success("Customer Updated Successfully")
-        );
+    @DeleteMapping("/{customerId}/nominee/{nomineeId}")
+    public ResponseEntity<Map<String, Object>> deleteNominee(
+    		@PathVariable Long customerId,
+    		@PathVariable Long nomineeId) {
+    	
+    	
+    	
+    	try {
+    		customerService.deleteNominee(customerId, nomineeId);
+    		
+    		Map<String, Object> response=new LinkedHashMap<>();
+    		response.put("status", "SUCCESS");
+    		response.put("message", "Nominee deleted Successfully");
+    		
+    		return ResponseEntity.ok(response);
+    			
+    	}catch(RuntimeException e) {
+    		
+    		Map<String, Object> response=new LinkedHashMap<>();
+    		response.put("status", "FAILED");
+    		response.put("message", e.getMessage());
+    		
+    		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    	}
     }
-
-   
-
 }
 
 
