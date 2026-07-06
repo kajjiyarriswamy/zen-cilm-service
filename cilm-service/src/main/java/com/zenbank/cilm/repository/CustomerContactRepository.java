801dccd4916564adbb9fb8eb8140528b8068e041
Satishpatta <satishpatta555@gmail.com>
2026-07-06 12:53:15 +0530

    implemented GET API for get customer contact details by customerID

--- cilm-service/src/main/java/com/zenbank/cilm/repository/CustomerContactRepository.java
+++ cilm-service/src/main/java/com/zenbank/cilm/repository/CustomerContactRepository.java
@@ -1,10 +1,12 @@
 package com.zenbank.cilm.repository;
 
 import com.zenbank.cilm.entity.Customer;
+import com.zenbank.cilm.entity.CustomerAddress;
 import com.zenbank.cilm.entity.CustomerContact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +17,7 @@ public interface CustomerContactRepository extends JpaRepository<CustomerContact
     boolean existsByEmail(String email);
 
     Optional<CustomerContact> findByCustomer(Customer customer);
+    
+    List<CustomerContact> findByCustomerCustomerId(String customerId);    
+    
 }
