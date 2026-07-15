87f14c4bcf30f86cc87a6459d3194cd9e616a72d
scaler projects <abhishekitte11@gmail.com>
2026-07-15 16:16:06 +0530

    Added Get Passbook Request Details API

--- account-management-service/src/main/java/com/zenbank/ams/account_management_service/controller/AccountPassbookRequestController.java
+++ account-management-service/src/main/java/com/zenbank/ams/account_management_service/controller/AccountPassbookRequestController.java
@@ -3,6 +3,7 @@ package com.zenbank.ams.account_management_service.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zenbank.ams.account_management_service.dto.CreatePassbookRequest;
+import com.zenbank.ams.account_management_service.dto.PassbookRequestDetailsResponse;
 import com.zenbank.ams.account_management_service.dto.PassbookRequestResponse;
 import com.zenbank.ams.account_management_service.service.AccountPassbookRequestService;
 
@@ -25,7 +27,25 @@ public class AccountPassbookRequestController {
 			@PathVariable Long accountId,
 
 			@RequestBody CreatePassbookRequest request) {
+		PassbookRequestResponse response = service.createRequest(accountId, request);
 
-		return new ResponseEntity<>(service.createRequest(accountId, request), HttpStatus.CREATED);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+		
 	}
+	
+	
+	
+	@GetMapping("/{accountId}/passbook-requests/{passbookRequestId}")
+	public ResponseEntity<PassbookRequestDetailsResponse> getPassbookRequest(
+			
+			@PathVariable Long accountId,
+			@PathVariable Long passbookRequestId){
+		 
+		PassbookRequestDetailsResponse response =
+	            service.getPassbookRequest(accountId, passbookRequestId);
+
+	    return ResponseEntity.ok(response);
+	}
+	
 }
