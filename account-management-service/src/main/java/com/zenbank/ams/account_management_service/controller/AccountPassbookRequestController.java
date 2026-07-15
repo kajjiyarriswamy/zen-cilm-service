2e50cc4bb76cf47d09e7a58af97637e32e916116
scaler projects <abhishekitte11@gmail.com>
2026-07-15 23:29:33 +0530

    Added Update Passbook Request API

--- account-management-service/src/main/java/com/zenbank/ams/account_management_service/controller/AccountPassbookRequestController.java
+++ account-management-service/src/main/java/com/zenbank/ams/account_management_service/controller/AccountPassbookRequestController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,23 +30,27 @@ public class AccountPassbookRequestController {
 			@RequestBody CreatePassbookRequest request) {
 		PassbookRequestResponse response = service.createRequest(accountId, request);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 
-		
 	}
-	
-	
-	
+
 	@GetMapping("/{accountId}/passbook-requests/{passbookRequestId}")
 	public ResponseEntity<PassbookRequestDetailsResponse> getPassbookRequest(
-			
-			@PathVariable Long accountId,
-			@PathVariable Long passbookRequestId){
-		 
-		PassbookRequestDetailsResponse response =
-	            service.getPassbookRequest(accountId, passbookRequestId);
 
-	    return ResponseEntity.ok(response);
+			@PathVariable Long accountId, @PathVariable Long passbookRequestId) {
+
+		PassbookRequestDetailsResponse response = service.getPassbookRequest(accountId, passbookRequestId);
+
+		return ResponseEntity.ok(response);
 	}
-	
+
+	@PutMapping("/{accountId}/passbook-requests/{passbookRequestId}")
+	public ResponseEntity<PassbookRequestResponse> updatePassbookRequest(@PathVariable Long accountId,
+			@PathVariable Long passbookRequestId, @RequestBody CreatePassbookRequest request) {
+
+		PassbookRequestResponse response = service.updatePassbookRequest(accountId, passbookRequestId, request);
+
+		return ResponseEntity.ok(response);
+	}
+
 }
