4064312fa01b75fb998de2f1dea70ac0fa2286fa
Sudharshan0903 <gaddamsudharshan09@gmail.com>
2026-07-22 10:04:20 +0530

    Implemented Update Interest Configuration API

--- account-management-service/src/main/java/com/zenbank/ams/account_management_service/controller/InterestConfigurationController.java
+++ account-management-service/src/main/java/com/zenbank/ams/account_management_service/controller/InterestConfigurationController.java
@@ -2,13 +2,17 @@ package com.zenbank.ams.account_management_service.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zenbank.ams.account_management_service.dto.CreateInterestConfigurationRequest;
 import com.zenbank.ams.account_management_service.dto.InterestConfigurationResponse;
+import com.zenbank.ams.account_management_service.dto.UpdateInterestConfigurationRequest;
+import com.zenbank.ams.account_management_service.dto.UpdateInterestConfigurationResponseDto;
 import com.zenbank.ams.account_management_service.service.InterestConfigurationService;
 
 @RestController
@@ -20,5 +24,13 @@ public class InterestConfigurationController {
 	@PostMapping
 	public ResponseEntity<InterestConfigurationResponse>create(@RequestBody CreateInterestConfigurationRequest request){
 		return ResponseEntity.ok(intresetConfigurationService.createInterestConfiguration(request));
+	}
+	@PutMapping("/{interestId}")
+	public ResponseEntity<UpdateInterestConfigurationResponseDto> updateInterestConfiguration(
+	        @PathVariable Long interestId,
+	        @RequestBody UpdateInterestConfigurationRequest request) {
+
+	    return ResponseEntity.ok(
+	            intresetConfigurationService.updateInterestConfiguration(interestId, request));
 	}
 }
