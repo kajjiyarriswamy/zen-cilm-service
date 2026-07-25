623b64738cee8915e745d9d7552d113c6e6f3b02
Gnana1999G <gattamgnanendra97@gmail.com>
2026-07-26 00:04:30 +0530

    Implement deposit transaction details API

---
+++ deposit-service/src/main/java/com/zenbank/deposit_service/exception/DepositNotFoundException.java
@@ -0,0 +1,12 @@
+package com.zenbank.deposit_service.exception;
+
+public class DepositNotFoundException extends RuntimeException{
+	
+	public DepositNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
+
+
+
