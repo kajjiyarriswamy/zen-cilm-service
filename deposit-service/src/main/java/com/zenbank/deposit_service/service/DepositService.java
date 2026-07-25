203725fc66959da259a2e5c96b3c66e66b6438e3
Ganesh Guttula <ganeshnani24@gmail.com>
2026-07-25 12:39:10 +0530

    CreationDeposit

--- deposit-service/src/main/java/com/zenbank/deposit_service/service/DepositService.java
+++ deposit-service/src/main/java/com/zenbank/deposit_service/service/DepositService.java
@@ -3,9 +3,12 @@ package com.zenbank.deposit_service.service;
 import com.zenbank.deposit_service.dto.CreateDepositRequest;
 import com.zenbank.deposit_service.dto.DepositResponse;
 
+
 public class DepositService {
-	 public DepositResponse executeDeposit(CreateDepositRequest request) {
-		return null;
-		 
-	 }
+
+    
+    public DepositResponse executeDeposit(CreateDepositRequest request) {
+        
+        return null;
+    }
 }
